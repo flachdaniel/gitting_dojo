492c634632a36eb7203883c5aa7d79148f7086dd
Flach Daniel <flachdaniel77@gmail.com>
2017-07-11 09:37:50 +0200

    Joke function added

--- gittie.py
+++ gittie.py
@@ -1,2 +1,5 @@
 def introduce():
     print("Hello I'm Gittie!")
+
+def joke():
+    print("What's the difference between a Taliban outpost and a Pakistani elementary school? I don't know, I just fly the drone!")
